59244bd68665f17979908a34f8f27e1405d08856
bigdata02 <bigdata02@211.48.228.42>
2021-07-28 17:12:08 +0900

    dao, dto, joinservice min

--- OneProject/src/main/java/Model/WebMemberDTO.java
+++ OneProject/src/main/java/Model/WebMemberDTO.java
@@ -6,6 +6,37 @@ public class WebMemberDTO {
 	private String pw;
 	private String nickName;
 	private String areaInterest;
+	
+	public WebMemberDTO(String email, String pw, String nickName, String areaInterest) {
+		this.email = email;
+		this.pw = pw;
+		this.nickName = nickName;
+		this.areaInterest = areaInterest;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getAreaInterest() {
+		return areaInterest;
+	}
+	public void setAreaInterest(String areaInterest) {
+		this.areaInterest = areaInterest;
+	}
 
 	
 }
