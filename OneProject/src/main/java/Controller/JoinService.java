f0d095f321c5c802562b81d91ae2c5439a0b7cd7
bigdata02 <bigdata02@211.48.228.42>
2021-07-31 15:38:19 +0900

    선생님께서 보시느라 조금 바뀐사항인데 돌아가는건 똑같습니다 min

--- OneProject/src/main/java/Controller/JoinService.java
+++ OneProject/src/main/java/Controller/JoinService.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,37 +13,41 @@ import javax.servlet.http.HttpSession;
 import Model.WebMemberDAO;
 import Model.WebMemberDTO;
 
-
+/**
+ * Servlet implementation class JoinService
+ */
 @WebServlet("/JoinService")
 public class JoinService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		
+
 		String email = request.getParameter("email");
 		String pw = request.getParameter("pw");
 		String nick = request.getParameter("nick");
-		String areaInterest = request.getParameter("areaInterest1")+request.getParameter("areaInterest2")+request.getParameter("areaInterest3");
-		
+		String areaInterest = request.getParameter("areaInterest1") + request.getParameter("areaInterest2")
+				+ request.getParameter("areaInterest3");
+
 		System.out.println(email);
-		
+
 		WebMemberDAO dao = new WebMemberDAO();
-		WebMemberDTO dto = new WebMemberDTO(email, pw, nick, areaInterest);		
-		
+		WebMemberDTO dto = new WebMemberDTO(email, pw, nick, areaInterest);
+
 		int cnt = dao.join(dto);
 		System.out.println(cnt);
 
-		if(cnt >0) {
+		if (cnt > 0) {
 			System.out.println("저장성공");
 			HttpSession session = request.getSession();
 			session.setAttribute("email", email);
 
-		}else {
+		} else {
 			System.out.println("ㅅㅂ");
 		}
+
 		response.sendRedirect("iLBiMain.jsp");
 
 	}
